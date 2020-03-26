c0263c40bfc051760bd196161eb322e41cdf0a66
James <admin@James.local>
2020-03-26 15:28:23 +0300

    redone server

--- server.js
+++ server.js
@@ -1,60 +1,31 @@
+//
+
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
+const bodyParser = require("body-parser");
+require("dotenv/config");
 
-// Models
-let Post = require("./models/Posts");
-console.log("Postsss", Post);
-// Initialize app
 const app = express();
 
-// Connecting to mongoDB
+// routes
+const postsRoutes = require("./Routes/post");
 
-mongoose.connect("mongodb://localhost/blogDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+app.use("/people", postsRoutes);
+app.use(bodyParser.json());
 
-let db = mongoose.connection;
-// Check connection success
-db.on("open", () => {
-  console.log("Connected to MongoDB");
-});
+// db connection
 
-// Check connection errors
-
-db.on("error", err => {
-  console.log(err);
-});
-
-// Pass in the folder that will keep the view
-app.set("views", path.join(__dirname, "views"));
-// Set view engine to pug
-app.set("view engine", "pug");
-
-// Home Route
-app.get("/", (req, res) => {
-  Post.find({}, (err, postsArray) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("index", {
-        title: "Hello",
-        posts: postsArray
-      });
-    }
+// server
+mongoose
+  .connect(
+    `mongodb+srv://james:glJOadOY0ALfbR06@cluster0-fhwxu.mongodb.net/homes-dev-db?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("connected");
+    app.listen(5000);
+  })
+  .catch(err => {
+    console.log("Shida");
+    throw err;
   });
-});
-
-// Add post
-
-app.get("/posts/add", (req, res) => {
-  res.render("add_post", {
-    title: "Add Post"
-  });
-});
-
-// Start server
-app.listen(5000, () => {
-  console.log("Server running on 5000");
-});
