3cf9a5eddb0dbb44cf209eed9d063a9fc48ee279
James <admin@James.local>
2020-03-26 17:49:57 +0300

    Edited posts

--- Routes/post.js
+++ Routes/post.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Posts");
 
+// Get all posts
 router.get("/", async (req, res) => {
   try {
     const posts = await Post.find();
@@ -12,6 +13,7 @@ router.get("/", async (req, res) => {
   }
 });
 
+// add a post
 router.post("/add", async (req, res) => {
   const newPost = new Post({
     title: req.body.title,
@@ -27,6 +29,7 @@ router.post("/add", async (req, res) => {
   }
 });
 
+// get a specific post
 router.get("/:postId", async (req, res) => {
   try {
     const post = await Post.findById(req.params.postId);
@@ -36,4 +39,42 @@ router.get("/:postId", async (req, res) => {
   }
 });
 
+//delete a post
+router.delete("/:postId", async (req, res) => {
+  try {
+    const exists = await Post.findById(req.params.postId);
+    if (exists) {
+      const deletedPost = await Post.remove({ _id: req.params.postId });
+      res.json({ posts: deletedPost, message: "deleted" });
+    } else {
+      res.json({ message: `sorry :${req.params.postId} was not found` });
+    }
+  } catch (err) {
+    res.json({ message: `Error deleting :${req.params.postId}` });
+  }
+});
+
+// Edit Post
+router.patch("/:postId", async (req, res) => {
+  try {
+    const exists = await Post.findById(req.params.postId);
+    if (exists) {
+      const editedPost = await Post.updateOne(
+        { _id: req.params.postId },
+        {
+          $set: {
+            title: req.body.title,
+            author: req.body.author,
+            body: req.body.body
+          }
+        }
+      );
+      res.json({ editedPost, message: "Edited" });
+    } else {
+      res.json({ message: `sorry :${req.params.postId} was not found` });
+    }
+  } catch (err) {
+    res.json({ message: `Error Editing :${req.params.postId}` });
+  }
+});
 module.exports = router;
