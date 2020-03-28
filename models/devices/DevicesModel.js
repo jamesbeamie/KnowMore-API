d0a62d5b12288d1fb2b4fb0550e577ddc6835156
James <admin@James.local>
2020-03-28 16:40:40 +0300

    restructure device model and implement file upload

---
+++ models/devices/DevicesModel.js
@@ -0,0 +1,36 @@
+let mongoose = require("mongoose");
+
+// Posts Schema
+
+let postSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  model: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  },
+  productionYear: {
+    type: Number,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  deviceImage: {
+    type: String,
+    require: true
+  }
+});
+
+module.exports = mongoose.model("Device", postSchema);
