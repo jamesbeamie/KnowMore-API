499aa57f69077a8ec8f0e8164b4d6be9a6d56706
gathoni-k <maryg.kahiga@gmail.com>
2020-04-28 05:22:52 +0300

    Create update review route

--- Routes/reviews/reviewsRoutes.js
+++ Routes/reviews/reviewsRoutes.js
@@ -27,4 +27,19 @@ router.post("/:deviceId", authMiddleware, async (req, res) => {
   }
 });
 
+// edit review
+router.patch('/:reviewId', async(req, res) => {
+  let review = await Review.findById(req.params.reviewId)
+  if(review) {
+    try {
+      const editedReview = req.body.review
+      review.review = editedReview
+      await review.save()
+    } catch (error) {
+      res.status(400).json({ message: "Error editing review"})
+    }
+  } else {
+    res.status(404).json({ message: "Review not found"})
+  } 
+})
 module.exports = router;
