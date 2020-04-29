d6434aae33ca27d443066c2c7fd80c9124deb08d
James <admin@James.local>
2020-04-29 11:25:44 +0300

    rebase

--- models/users/UserModel.js
+++ models/users/UserModel.js
@@ -34,17 +34,15 @@ const userSchema = mongoose.Schema({
     email: {
       type: String,
     },
+    verificationTkn: { type: String },
   },
-    required: true,
-  },
-  verificationTkn: { type: String },
 });
 
 // hash password before save
 userSchema.pre("save", async function (next) {
   try {
     if (!this.password) next();
-    if (!this.isModified("password")) next();// only hash if password is modified
+    if (!this.isModified("password")) next(); // only hash if password is modified
     const salt = await bcrypt.genSalt(12);
     const hash = await bcrypt.hash(this.password, salt);
     this.password = hash;
