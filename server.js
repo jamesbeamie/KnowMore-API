3264230f482c33badae891c8772d6d80cf876ab4
James <admin@James.local>
2020-05-22 18:15:27 +0300

    remove unused imports in server file

--- server.js
+++ server.js
@@ -6,18 +6,7 @@ const logger = require("morgan");
 require("dotenv/config");
 const passport = require("passport");
 
-//module imports
 // routes imports
-const devicesRoutes = require("./Routes/Devices/devices");
-const userRoutes = require("./Routes/users/registration/User");
-const loginRoutes = require("./Routes/users/login/Login");
-const ratingRoutes = require("./Routes/rating/Rating");
-const likeDislikeRoutes = require("./Routes/Devices/likeDislikeDevice");
-const reviewRoutes = require("./Routes/reviews/reviewsRoutes");
-const pwdResetRoutes = require("./Routes/users/pwdReset/passwordReset");
-const favouriteRoutes = require("./Routes/Devices/favouriteDevice");
-const inviteRoutes = require("./Routes/invite/invite");
-// refactored routes with versioning
 const apiRoutes = require("./Routes");
 
 // initialize app
@@ -32,27 +21,14 @@ app.use(cors());
 app.use(logger("dev"));
 
 // routes
-// app.use("/devices", devicesRoutes);
-// app.use("/users", userRoutes);
-// app.use("/auth", loginRoutes);
-// app.use("/rating", ratingRoutes);
-// app.use("/reviews", reviewRoutes);
-// app.use("/liking", likeDislikeRoutes);
-// app.use("/password-reset", pwdResetRoutes);
-// app.use("/favourites", favouriteRoutes);
-// app.use("/invite", inviteRoutes);
-// Versioned
 app.use("/api/V1", apiRoutes);
 
 // server
 mongoose
-  .connect(
-    // atlas
-    // `${process.env.ATLAS_DB}`,
-    //local DB
-    `${process.env.LOCAL_DB}`,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(`${process.env.LOCAL_DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("connected");
     app.listen(`${process.env.PORT}` || 5001);
